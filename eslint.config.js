c4d23148591eecce68a630c94274f4fad8acf991
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2026-02-25 16:45:09 -0700

    Add linting

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  ...pluginVue.configs['flat/recommended'],
+  ...vueTsEslintConfig(),
+  prettierConfig,
+]
