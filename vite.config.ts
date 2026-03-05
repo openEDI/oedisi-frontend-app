dc9d4f7ae18e407841f400280fb12c3f44555ab7
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2026-03-05 09:07:09 -0700

    Update node version and add github actions work CI and pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/oedisi-frontend-app/' : '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
